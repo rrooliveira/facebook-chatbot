afefe1c6602420889350c85bfa034404b12c91bf
rodrigo.ruyoliveira <rodrigo.oliveira.globant@estadao.com>
2022-08-04 11:30:08 -0300

    Updating controller requests

--- app/Http/Controllers/ChatBotController.php
+++ app/Http/Controllers/ChatBotController.php
@@ -38,10 +38,10 @@ class ChatBotController extends Controller
         $message = $senderMessage->getMessage();
 
         $text = new Text($recipientId);
-        $file = new File($recipientId);
-        $image = new Image($recipientId);
-        $audio = new Audio($recipientId);
-        $video = new Video($recipientId);
+//        $file = new File($recipientId);
+//        $image = new Image($recipientId);
+//        $audio = new Audio($recipientId);
+//        $video = new Video($recipientId);
         $httpClient = new Guzzle(config('chatbotfacebook.pageAccessToken'));
 
         try {
@@ -59,9 +59,9 @@ class ChatBotController extends Controller
 //            $file->setMessage('https://www.php.net/distributions/php-8.1.9.tar.gz');
 //            $httpClient->post($file->getMessage());
 //
-            //IMAGE
-            $image->setMessage('https://www.php.net/images/logos/php-logo.svg');
-            $httpClient->post($image->getMessage());
+//            //IMAGE
+//            $image->setMessage('https://www.php.net/images/logos/php-logo.svg');
+//            $httpClient->post($image->getMessage());
 
 //            //AUDIO
 //            $audio->setMessage('');
